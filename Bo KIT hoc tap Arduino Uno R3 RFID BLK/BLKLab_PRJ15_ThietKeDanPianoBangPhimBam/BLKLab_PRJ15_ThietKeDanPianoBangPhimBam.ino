6606dde1ce05b26fb9a271c25bc20b045a6f411a
BanLinhKien <124423917+BanLinhKien@users.noreply.github.com>
2023-09-29 14:15:18 +0700

    Update BLKLab_PRJ15_ThietKeDanPianoBangPhimBam.ino

--- Bo KIT hoc tap Arduino Uno R3 RFID BLK/BLKLab_PRJ15_ThietKeDanPianoBangPhimBam/BLKLab_PRJ15_ThietKeDanPianoBangPhimBam.ino
+++ Bo KIT hoc tap Arduino Uno R3 RFID BLK/BLKLab_PRJ15_ThietKeDanPianoBangPhimBam/BLKLab_PRJ15_ThietKeDanPianoBangPhimBam.ino
@@ -31,19 +31,22 @@ const int p5 = 6;
 const int p6 = 5;  
 const int p7 = 4;  
 const int p8 = 3;  
-
+const int p9 = 11; 
+const int p10 =12 ; 
 const int OFF = LOW;                                                // Chế độ tắt (độ thấp)
 const int ON = HIGH;                                                // Chế độ bật (độ cao)
 
 // Định nghĩa các âm thanh bằng tần số (đơn vị là Hz)
-const int NOTE_C4 = 262;
-const int NOTE_D4 = 294;
-const int NOTE_E4 = 330;
-const int NOTE_F4 = 349;
-const int NOTE_G4 = 392;
-const int NOTE_A4 = 440;
-const int NOTE_B4 = 494;
-const int NOTE_C5 = 523;
+const int NOTE_do = 820;
+const int NOTE_re = 880;
+const int NOTE_mi = 956;
+const int NOTE_pha = 1014;
+const int NOTE_sol = 1136;
+const int NOTE_la =  1275;
+const int NOTE_si  = 1432;
+const int NOTE_do2 = 1519;
+const int NOTE_re2 = 1700;
+const int NOTE_mi2 = 1915;
 
 // Hàm phát ra âm thanh
 void playSound(int frequency) {
@@ -62,6 +65,8 @@ void setup() {
   pinMode(p6, INPUT_PULLUP);
   pinMode(p7, INPUT_PULLUP);
   pinMode(p8, INPUT_PULLUP);
+  pinMode(p9, INPUT_PULLUP);
+  pinMode(p10, INPUT_PULLUP);
 
   // Khởi tạo Serial Monitor
   Serial.begin(9600);
@@ -70,35 +75,43 @@ void setup() {
 void loop() {
   // Kiểm tra các phím bấm
   if (digitalRead(p1) == OFF) {
-    playSound(NOTE_C4);                                             // Phát âm thanh tương ứng với phím 1
+    playSound(NOTE_do);                                             // Phát âm thanh tương ứng với phím 1
     Serial.println("Phim 1 duoc nhan");
   }
   if (digitalRead(p2) == OFF) {
-    playSound(NOTE_D4);                                             // Phát âm thanh tương ứng với phím 2
+    playSound(NOTE_re);                                             // Phát âm thanh tương ứng với phím 2
     Serial.println("Phim 2 duoc nhan");
   }
   if (digitalRead(p3) == OFF) {
-    playSound(NOTE_E4);                                             // Phát âm thanh tương ứng với phím 3
+    playSound(NOTE_mi);                                             // Phát âm thanh tương ứng với phím 3
     Serial.println("Phim 3 duoc nhan");
   }
   if (digitalRead(p4) == OFF) {
-    playSound(NOTE_F4);                                             // Phát âm thanh tương ứng với phím 4
+    playSound(NOTE_pha);                                             // Phát âm thanh tương ứng với phím 4
     Serial.println("Phim 4 duoc nhan");
   }
   if (digitalRead(p5) == OFF) {
-    playSound(NOTE_G4);                                             // Phát âm thanh tương ứng với phím 5
+    playSound(NOTE_sol);                                             // Phát âm thanh tương ứng với phím 5
     Serial.println("Phim 5 duoc nhan");
   }
   if (digitalRead(p6) == OFF) {
-    playSound(NOTE_A4);                                             // Phát âm thanh tương ứng với phím 6
+    playSound(NOTE_la);                                             // Phát âm thanh tương ứng với phím 6
     Serial.println("Phim 6 duoc nhan");
   }
   if (digitalRead(p7) == OFF) {
-    playSound(NOTE_B4);                                             // Phát âm thanh tương ứng với phím 7
+    playSound(NOTE_si);                                             // Phát âm thanh tương ứng với phím 7
     Serial.println("Phim 7 duoc nhan");
   }
   if (digitalRead(p8) == OFF) {
-    playSound(NOTE_C5);                                             // Phát âm thanh tương ứng với phím 8
+    playSound(NOTE_do2);                                             // Phát âm thanh tương ứng với phím 8
+    Serial.println("Phim 8 duoc nhan");
+  }
+  if (digitalRead(p9) == OFF) {
+    playSound(NOTE_re2);                                             // Phát âm thanh tương ứng với phím 8
+    Serial.println("Phim 8 duoc nhan");
+  }
+  if (digitalRead(p10) == OFF) {
+    playSound(NOTE_mi2);                                             // Phát âm thanh tương ứng với phím 8
     Serial.println("Phim 8 duoc nhan");
   }
 }
