393d85b59dd0b0eec73d95bb1aaafa18bb729ae5
BanLinhKien <banlinhkien.vn@gmail.com>
2024-10-09 16:29:10 +0700

    update

--- Bo KIT hoc tap Arduino Uno R3 RFID BLK/BLKLab_PRJ06_Cach hien thi thong tin du lieu len LCD1602/BLKLab_So_Do_Dau_Noi+Video_Huong_Dan/Video_huong_dan.c
+++
@@ -1,5 +0,0 @@
-*/
-Youtube: https://www.youtube.com/shorts/4kUr1YKePUM
-Tiktok: https://www.tiktok.com/@banlinhkienretail/video/7270699295504895240?lang=vi-VN
-Facebook: https://www.facebook.com/reel/702340381728748
-*/
