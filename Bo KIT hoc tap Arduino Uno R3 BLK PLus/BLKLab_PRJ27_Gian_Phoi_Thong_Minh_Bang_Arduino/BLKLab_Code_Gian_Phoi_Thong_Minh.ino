7205ffe8025f81b53bc4c4563e2c8a70447b2dba
BanLinhKien <banlinhkien.vn@gmail.com>
2024-11-13 11:27:32 +0700

    update

--- Bo KIT hoc tap Arduino Uno R3 BLK PLus/BLKLab_PRJ27_Gian_Phoi_Thong_Minh_Bang_Arduino/BLKLab_Code_Gian_Phoi_Thong_Minh.ino
+++ Bo KIT hoc tap Arduino Uno R3 BLK PLus/BLKLab_PRJ27_Gian_Phoi_Thong_Minh_Bang_Arduino/BLKLab_Code_Gian_Phoi_Thong_Minh.ino
@@ -7,9 +7,11 @@
 
 #define RAIN  2
 #define SERVO 9
+#define GOC 90  // thay đổi góc quay
 Servo myservo;  // tạo đối tượng servo để điều khiển servo
 
 int pos = 0;    // tạo biến góc của servo
+int t = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -19,18 +21,28 @@ void setup() {
 }
 
 void loop() {
+  
   if (digitalRead(2) == LOW){
       Serial.println("Co MUA!!!");
-      for (pos = 0; pos <= 180; pos += 1) { // góc quay từ 0 đến 180 độ
-        myservo.write(pos);              
-        delay(20);                      
+      if (t == 0){
+        for (pos = 0; pos <= GOC; pos += 1) { // góc quay từ 0 đến 180 độ
+          myservo.write(pos);              
+          delay(20);  
+          t = 1;                    
+        }
       }
+      else myservo.write(GOC); 
   }
-  else{
+  else {
+      
       Serial.println("Khong MUA");
-      for (pos = 180; pos >= 0; pos -= 1) { // góc quay từ 180 đến 0 độ
-        myservo.write(pos);              
-        delay(20);                       
+      if (t == 1){
+        for (pos = GOC; pos >= 0; pos -= 1) { // góc quay từ 180 đến 0 độ
+          myservo.write(pos);              
+          delay(20);  
+          t = 0;                     
+        }
       }
+      else myservo.write(0); 
   }
 }
